27c8c156b7cd4cc9f1a8e45940968011df89ae45
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-05-11 15:31:23 +0200

    chore: dev database seed

---
+++ prisma/seed.ts
@@ -0,0 +1,82 @@
+import { type Post, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function clearDataBase() {
+  await prisma.like.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.post.deleteMany();
+}
+
+async function generateUsers() {
+  const joe = await prisma.user.create({
+    data: {
+      email: 'joe@doe.com',
+      name: 'Joe Doe',
+      image: 'https://cdn.discordapp.com/embed/avatars/3.png',
+      posts: {
+        create: [
+          { message: 'this is first tweet' },
+          { message: 'My second tweet' },
+          {
+            message:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident accusantium doloribus blanditiis modi vel, nemo enim pariatur nobis! Quae, perspiciatis eligendi praesentium autem aspernatur ipsa recusandae voluptatum accusamus temporibus.',
+          },
+        ],
+      },
+    },
+    select: { id: true, name: true, posts: true, email: true },
+  });
+
+  const jane = await prisma.user.create({
+    data: {
+      email: 'jane@doe.com',
+      name: 'Jane Doe',
+      image: 'https://cdn.discordapp.com/embed/avatars/3.png',
+      posts: {
+        create: [
+          { message: 'My name is Jane doe Gorge bush did (tweet deleted)' },
+          { message: "I'm a reformed woman now" },
+          {
+            message:
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident accusantium doloribus blanditiis modi vel, nemo enim pariatur nobis! Quae, perspiciatis eligendi praesentium autem aspernatur ipsa recusandae voluptatum accusamus temporibus.',
+          },
+        ],
+      },
+    },
+    select: { id: true, name: true, posts: true, email: true },
+  });
+  return { joe, jane };
+}
+
+type UserWithPosts = {
+  id: string;
+  name: string | null;
+  email: string | null;
+  posts: Post[];
+};
+
+async function likeTweets(jane: UserWithPosts, joe: UserWithPosts) {
+  prisma.post.update({
+    where: { id: jane.posts[0].id },
+    data: { likes: { create: [{ userId: jane.id }, { userId: joe.id }] } },
+  });
+}
+
+async function main() {
+  await clearDataBase();
+
+  const { jane, joe } = await generateUsers();
+
+  likeTweets(jane, joe);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
