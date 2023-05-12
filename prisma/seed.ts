90936cf80ceee8911289ea6209f5cccd391f0c21
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-05-12 08:57:33 +0200

    chore change prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,9 +3,9 @@ import { type Post, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function clearDataBase() {
-  await prisma.like.deleteMany();
-  await prisma.user.deleteMany();
   await prisma.post.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.like.deleteMany();
 }
 
 async function generateUsers() {
