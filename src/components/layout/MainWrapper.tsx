a5703647c0805f2f76f1e11843ef989e53ac68ef
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-05-22 23:48:01 +0200

    feat: conditionally show bottom border

--- src/components/layout/MainWrapper.tsx
+++ src/components/layout/MainWrapper.tsx
@@ -1,16 +1,24 @@
+import { cn } from '@/lib/cn';
 import React, { PropsWithChildren } from 'react';
 
 type MainWrapperProps = PropsWithChildren & {
   headerComponent: React.ReactNode;
+  showBorder?: boolean;
 };
 
 export default function MainWrapper({
   children,
   headerComponent,
+  showBorder = true,
 }: MainWrapperProps) {
   return (
     <div className='refative flex h-full min-h-screen w-full flex-col border-x border-x-gray-700'>
-      <div className='sticky left-0 right-0 top-0 z-10 w-full border-b border-b-gray-700 bg-background/10 backdrop-blur-lg'>
+      <div
+        className={cn(
+          showBorder && 'border-b',
+          'sticky left-0 right-0 top-0 z-10 w-full border-b-gray-700 bg-background/10 backdrop-blur-lg'
+        )}
+      >
         {headerComponent}
       </div>
       {children}
