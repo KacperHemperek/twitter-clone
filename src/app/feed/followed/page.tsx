4764dd4c38c4a6b00afc38bc307f9f3492f8c278
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-01 16:35:17 +0200

    feat: add padding to comming soon followed page

--- src/app/feed/followed/page.tsx
+++ src/app/feed/followed/page.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 export default function FollowedFeed() {
   return (
-    <div className="flex items-center justify-center text-2xl">
-      {' '}
+    <div className="flex items-center justify-center text-2xl px-6 py-20">
       Comming Soon...
     </div>
   );
