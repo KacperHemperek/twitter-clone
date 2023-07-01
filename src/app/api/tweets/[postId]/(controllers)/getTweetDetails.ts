f72284a25d1616b4942c58d3cd729e9e5ab715df
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-01 20:33:28 +0200

    refactor: remove (routes) folders from

--- src/app/api/tweets/[postId]/(controllers)/getTweetDetails.ts
+++ src/app/api/tweets/[postId]/(controllers)/getTweetDetails.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-import { getTweetDetails } from '../../../(services)/tweet.service';
+import { getTweetDetails } from '../../(services)/tweet.service';
 
 import {
   ServerError,
