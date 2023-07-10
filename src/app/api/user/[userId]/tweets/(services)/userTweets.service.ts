b970fe7859c4b37926aa37af9cd2853a18098d4f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-10 19:27:02 +0200

    chore: remove unnecessary function

--- src/app/api/user/[userId]/tweets/(services)/userTweets.service.ts
+++ src/app/api/user/[userId]/tweets/(services)/userTweets.service.ts
@@ -4,40 +4,6 @@ import { prisma } from '@/db/prisma';
 
 const TWEET_LIMIT = 10;
 
-export async function getUsersTweets(userId: string, page: number) {
-  try {
-    const posts = await prisma.post.findMany({
-      where: {
-        OR: [
-          {
-            parentId: null,
-            authorId: userId,
-          },
-          {
-            retweets: { some: { userId } },
-          },
-        ],
-      },
-      select: {
-        author: true,
-        createdAt: true,
-        id: true,
-        message: true,
-        likes: true,
-        comments: { select: { id: true } },
-        retweets: true,
-      },
-      orderBy: { createdAt: 'desc' },
-      take: TWEET_LIMIT,
-      skip: (page - 1) * TWEET_LIMIT,
-    });
-
-    return posts;
-  } catch (e) {
-    throw new ServerError(500, 'There was a problem retrieving feed data');
-  }
-}
-
 export async function getUserTweetsNew(userId: string, page: number) {
   try {
     const posts = await prisma.$queryRaw`
