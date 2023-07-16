07aa91bcd8c24c9f4674e9b3ab76ae1902d60c65
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-16 22:59:06 +0200

    feat: update base loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,14 @@
 import { TwitterIcon } from 'lucide-react';
 import React from 'react';
 
+import MainWrapper from '@/components/layout/MainWrapper';
+
 export default function RootLoading() {
   return (
-    <div className="w-screen h-screen bg-background flex items-center justify-center">
-      <TwitterIcon className="text-white animate-pulse" />
-    </div>
+    <MainWrapper>
+      <div className="h-screen bg-background flex items-center justify-center">
+        <TwitterIcon className="text-white animate-pulse w-12 h-12" />
+      </div>
+    </MainWrapper>
   );
 }
