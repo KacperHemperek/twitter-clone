26eceff21290bd22f72c1cad971bda36d6e97a0f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-07-19 21:10:53 +0200

    feat: add conditional border bottom to FeedNavigation

--- src/components/common/feed-navigation/FeedNavigation.tsx
+++ src/components/common/feed-navigation/FeedNavigation.tsx
@@ -4,13 +4,24 @@ import React from 'react';
 
 import FeedNavigationLink from '@/components/common/feed-navigation/FeedNavigationLink';
 
+import { cn } from '@/lib/cn';
+
 type FeedNavigationProps = {
   links: { label: string; href: string }[];
+  borderBottom?: boolean;
 };
 
-export default function FeedNavigation({ links }: FeedNavigationProps) {
+export default function FeedNavigation({
+  links,
+  borderBottom = false,
+}: FeedNavigationProps) {
   return (
-    <div className={`grid grid-cols-${links.length}`}>
+    <div
+      className={cn(
+        `grid grid-cols-${links.length}`,
+        borderBottom ? 'border-b border-gray-700' : 'border-none'
+      )}
+    >
       {links.map((link) => (
         <FeedNavigationLink {...link} key={link.href} />
       ))}{' '}
