4e6eae86533ae94c17c9e08e10d0543a811df4b2
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-10-30 23:06:46 +0100

    refactor: remove unused code commented out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,17 +31,6 @@ export default async function RootLayout({
             <div className="w-[calc(100vw-64px)] flex md:w-[calc(100vw-288px)] xl:w-full">
               {children}
             </div>
-
-            {/* )} */}
-            {/* {!session?.user && (
-              <div className="flex min-h-full w-full flex-col items-center justify-center gap-6 ">
-                <h1 className="max-w-[350px] text-center text-2xl font-bold">
-                  <span>You are not logged in. </span>
-                  <span>To use this app you need to log in</span>
-                </h1>
-                <SignInButton />
-              </div>
-            )} */}
           </main>
         </Providers>
       </body>
